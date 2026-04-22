2f9669e8634cc187bbc4a85de9465e93b715d601
Ahmed <ahmed@example.com>
2026-04-22 22:55:06 +0200

    connect home to main page

--- my-app/app/(main)/page.tsx
+++ my-app/app/(main)/page.tsx
@@ -2,7 +2,12 @@ import { MathCurrentCourses } from "@/components/sections/MathCurrentCourses";
 import { MathFeatures } from "@/components/sections/MathFeatures";
 import { MathHero } from "@/components/sections/MathHero";
 
-
 export default function HomePage() {
-  return <h1>TEST</h1>;
+  return (
+    <>
+      <MathHero />
+      <MathFeatures />
+      <MathCurrentCourses />
+    </>
+  );
 }
